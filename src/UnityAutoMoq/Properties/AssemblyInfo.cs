93b80436e7ebba137b002412609a4bd5accd0309
Azarkevich, Sergey <sergey.azarkevich@ihs.com>
2017-01-06 06:45:29 +0300

    Update copyrights

--- src/UnityAutoMoq/Properties/AssemblyInfo.cs
+++ src/UnityAutoMoq/Properties/AssemblyInfo.cs
@@ -4,6 +4,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("Automocking container using Microsoft Unity and Moq")]
 [assembly: AssemblyCompany("UnityAutoMoq")]
 [assembly: AssemblyProduct("UnityAutoMoq")]
-[assembly: AssemblyCopyright("Copyright (C) Thomas Pedersen")]
+[assembly: AssemblyCopyright("Copyright (C) Thomas Pedersen, Sergey Azarkevich, Henadz Linevich")]
 [assembly: AssemblyVersion("2.2.0.0")]
 
